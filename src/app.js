e29eafede86ef2aa7163395c2fd9498499bfa7f9
ramkumar-1819 <rohitram1819@gmail.com>
2021-05-15 18:24:47 +0530

    Done with no error

--- src/app.js
+++ src/app.js
@@ -2,15 +2,36 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
+function moreAboutHome(address, distanceFromTown, hasNeighbours) {
+    var result = "";
+    return result += (typeof address) + (typeof distanceFromTown) + (typeof hasNeighbours);
+}
 
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
 
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily) {
+    if ((typeof parents === "string") && (typeof noOfSiblings === "number") && (typeof isNuclearFamily === "boolean")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+
+function doesFriendExist(ageInText, ageInNumber) {
+    let temp;
+    if (isNaN(ageInText) == true) {
+        return ageInText;
+    } else {
+        return ageInNumber;
+    }
+}
 
 //Progression 4:
 //Lily gave Karen x sweets
@@ -19,9 +40,33 @@
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
 
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel) {
+    if (isNaN(totalNoOfSweets)) {
+        return "No sweets for Karen's friend";
+    } else if (totalNoOfSweets === 0) {
+        return 0;
+    } else {
+        totalNoOfSweets = totalNoOfSweets - sweetsConsumedByKaren - (sweetsConsumedInNMeters * metersToTravel);
+        if (totalNoOfSweets === 0) {
+            return 0;
+        } else {
+            return totalNoOfSweets;
+        }
+
+    }
+}
+
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+
+function convertToCelsius(fahrenheit) {
+    if (typeof fahrenheit === 'number') {
+        return Math.round((fahrenheit - 32) * (5 / 9));
+    } else {
+        return "Technical Error!";
+    }
+}
 
 //Progression 6:
 //Lily can now do multiple things to deal with this
@@ -31,6 +76,17 @@
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
 
+function aDifficultChoice(choice) {
+    if (choice === 1) {
+        return "Take her daughter to a doctor";
+    } else if (choice === -1) {
+        return "Break down and give up all hope";
+    } else if (choice === 'I give up') {
+        return "Refused to do anything for Karen";
+    } else {
+        return "Wasn't able to decide";
+    }
+}
 //Progression 7:
 //Lily realized that she'd hurt her daughter
 //All she wants now is for her to stop crying
@@ -39,3 +95,7 @@
 //Take all of Lily's strategies and concatenate them to a single var
 //Seperate the strategies by a single space
 //Return the length of the complete strategy
+
+function consoleKaren(strategies) {
+    return strategies.length;
+}
